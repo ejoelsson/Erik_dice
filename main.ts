5b304f32ff77721f4cdafbc924d31bc8f95859dd
Erik Joelsson <34108390+ejoelsson@users.noreply.github.com>
2025-11-09 20:52:42 +0100

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,60 @@
+let Siffra = 0
+function TärningsVisning (siffra: number) {
+    if (siffra == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (siffra == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (siffra == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (siffra == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (siffra == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (siffra == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    } else {
+    	
+    }
+}
+input.onGesture(Gesture.Shake, function () {
+    Siffra = randint(1, 6)
+    basic.showNumber(Siffra)
+    basic.pause(1000)
+    TärningsVisning(Siffra)
+})
